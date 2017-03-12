986fc3225b4437dd81e21c3fc6e6bb444d231c47
Yrocky <983272765@qq.com>
2017-03-13 02:02:39 +0800

    update my resume for 2017

--- README.md
+++
@@ -1,9 +0,0 @@
-Resume
-===
-
-
-This is a website with my resume .
-
-So, If your Company need an engineer type of iOS , [E-mail me](mailto:983272765@qq.com).
-
-Thx.
---
+++ main.js
@@ -0,0 +1,11 @@
+;(function load () {
+
+	var basis = document.getElementsByClassName('basis-section')[0];
+	var screenHeight = window.screen.height;
+
+	// basis.style = {
+	// 	'height' = 200+'px';	
+	// }
+	console.log(screenHeight)
+	// basis.style.height = 300+'px';
+})();
